e8c662e63849ddacf9bf9f58043e77aecf9a0248
TheSquid15 <viv@energycluster.dk>
2021-04-16 08:11:50 +0200

    added a solution for the collapsable element.

---
+++ collapsable.js
@@ -0,0 +1,14 @@
+var coll = document.getElementsByClassName("collapsible");
+    var i;
+    var desiredHeight = 1500;
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + desiredHeight + "px";
+            } 
+        });
+    }
